8b92ee202c49bccfb64e730219be9af1f7e3d050
Acaminos <caminosariel@gmail.com>
2021-09-15 01:37:52 -0300

    tp 3 no obligatorio

---
+++ trabajoPractico3/myapp/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+      <table>
+        <tr>
+          <td>Nombre:</td>
+          <td>Iphone 12 pro MAX</td>
+        </tr>
+        <tr>
+          <td>Descripcion:</td>
+          <td>128 Gb - 64 Mpx Cam</td>
+        </tr>
+        <tr>
+          <td>Precio:</td>
+          <td>U$S 1.000</td>
+        </tr>
+        <tr>
+          <td>SKU:</td>
+          <td>64734221111</td>
+        </tr>
+        <tr>
+          <td>Cant. Disponible:</td>
+          <td>10</td>
+        </tr>
+      </table>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
