65ba7f499e71adaf7ebb367242c1cc6f8b69256f
Acaminos <caminosariel@gmail.com>
2021-11-07 13:16:09 -0300

    registro finalizado

--- trabajoPracticoObligatorio2/tp/src/pages/registro.js
+++ trabajoPracticoObligatorio2/tp/src/pages/registro.js
@@ -12,25 +12,31 @@ function Registro(){
                     <div className="mb-3 row">
                         <label for="staticEmail" className="col-sm-4 col-form-label text-center">Ingrese su mail</label>
                         <div className="col-sm-8">
-                            <input type="text" className="form-control" {...register("email")}/>
+                            <input type="text" className="form-control" {...register("email",{required: true})}/>
                         </div>
                     </div>
+                    {errors.email && <span className="text-end pb-3">El campo es obligatorio</span>}
+
                     <div className="mb-3 row">
                         <label for="inputPassword" className="col-sm-4 col-form-label text-center">Ingrese su contrasena</label>
                         <div className="col-sm-8">
-                            <input type="password" className="form-control" {...register("password")}/>
+                            <input type="password" className="form-control" {...register("password",{required: true})}/>
                         </div>
                     </div>
+                    {errors.password && <span className="text-end pb-3">El campo es obligatorio</span>}
+
                     <div className="mb-3 row">
                         <label for="inputPassword" className="col-sm-4 col-form-label text-center">Ingrese nuevamente su contrasena</label>
                         <div className="col-sm-8">
-                            <input type="password" className="form-control" {...register("confirmPassword")}/>
+                            <input type="password" className="form-control" {...register("confirmPassword", {required: true})}/>
                         </div>
-                    </div>
-                    <div className="d-grid gap-2 col-6 mx-auto mt-4">
-                        <button className="btn btn-primary btn-sm" type="submit">Registrarme</button>
+                        {errors.confirmPassword && <span className="text-end">El campo es obligatorio</span>}
                     </div>
                 </div>
+                
+                <div className="d-grid gap-2 col-6 mx-auto mt-4 mb-4">
+                        <button className="btn btn-primary btn-sm" type="submit">Registrarme</button>
+                    </div>
             </form>
         </div>
     )
